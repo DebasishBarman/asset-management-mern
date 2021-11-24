c54dd0c50f5fd6204272944731e874086ef8deae
Debasish Barman <debdeva298@gmail.com>
2021-11-24 23:19:43 +0530

    Login and Signup UI created

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import {Container,Grid,Stack,Button,Typography,TextField} from '@mui/material'
+import { Container, Grid, Stack, Button, Typography, TextField, Box } from '@mui/material'
 const Login = () => {
     return (
-        <div>
-            
+        <div sx={{ border: '1px solid black' }}>
+            <Stack direction='row' justifyContent='space-between' sx={{ minWidth: '50vw', minHeight: '100vh' }}>
+                <Grid container sx={{ color: 'red', backgroundColor: 'red' }}>
+                    <h1>Image</h1>
+                </Grid>
+
+                <Grid container sx={{  minWidth: '50vw' }}
+                    alignItems="center" justifyContent='center'
+                >
+                    <Grid item md={8} sx={{ border: '1px' }} >
+                        <Typography sx={{ textAlign: 'left' }} variant="h4" component='p'>
+                            Log In
+                        </Typography>
+                        <Typography sx={{textAlign:'left', mb: 2 }} variant="subtitle" component='p'>
+                            Log in to your account
+                        </Typography>
+                        <TextField sx={{ mb: 2, borderRadius: 10 }} variant='filled' label='Username' fullWidth />
+                        <TextField sx={{ mb: 4 }} variant='filled' label='Password' fullWidth />
+
+                        <Button variant='contained' fullWidth sx={{p:2}}
+                        >
+                            Log in
+                        </Button>
+                    </Grid>
+
+
+                </Grid>
+
+            </Stack>
+
         </div>
+
     )
 }
 
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import { Container, Grid, Stack, Button, Typography, TextField, Box } from '@mui/material'
 const SignUp = () => {
     return (
-        <div>
-            
+        <div sx={{ border: '1px solid black' }}>
+            <Stack direction='row' justifyContent='space-between' sx={{ border: '1px solid black', minWidth: '50vw', minHeight: '100vh' }}>
+                <Grid container sx={{ color: 'red', backgroundColor: 'red' }}>
+                    <h1>Image</h1>
+                </Grid>
+
+                <Grid container sx={{ backgroundColor: 'yellow', minWidth: '50vw' }}
+                    alignItems="center" justifyContent='center'
+                >
+                    <Grid item md={8} sx={{ border: '1px' }} >
+                        <Typography sx={{ textAlign: 'right' }} variant="h4" component='p'>
+                            Log In
+                        </Typography>
+                        <Typography sx={{ mb: 2 }} variant="subtitle" component='p'>
+                            Log in to your account
+                        </Typography>
+                        <TextField sx={{ mb: 2, borderRadius: 10 }} variant='filled' label='Username' fullWidth />
+                        <TextField sx={{ mb: 4 }} variant='filled' label='Password' fullWidth />
+
+                        <Button variant='contained' fullWidth sx={{p:2}}
+                        >
+                            Log in
+                        </Button>
+                    </Grid>
+
+
+                </Grid>
+
+            </Stack>
+
         </div>
     )
 }
