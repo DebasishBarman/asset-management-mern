b2ad63dfb329d67addee253ff77b45e7f82a126b
Debasish Barman <debdeva298@gmail.com>
2021-11-25 23:10:00 +0530

    Dashboard Design
    Completed

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,29 +1,32 @@
 import React from 'react'
-import { Container, Grid, Stack, Divider, Typography } from '@mui/material'
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
+import {
+    Container, Grid, Stack, Divider, Typography,
+    Card, List, ListItem, ListItemButton, ListItemIcon, Box
+} from '@mui/material'
+
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/Inbox';
 import DraftsIcon from '@mui/icons-material/Drafts';
+import PixIcon from '@mui/icons-material/Pix';
 
 const Dashboard = () => {
     return (
-        <div style={{ background: '#4c566a' }}>
+        <div style={{ background: '#1D3557' }}>
             <Grid container lg={12}>
                 <Grid item lg={3} sx={{ minHeight: '100vh' }}>
-                    <Container sx={{ border: '1px solid black', p: 2, height: '100%' }}>
+                    <Container sx={{ p: 2, height: '100%' }}>
                         <nav
-                            style={{ border: '1px solid black',
-                             height: '100%',background:'#fff',
-                             borderRadius: 10, padding: 3, margin: 2 }}
+                            style={{
+                                border: '1px solid black',
+                                height: '99%', background: '#fff',
+                                borderRadius: 10, padding: 3, margin: 2
+                            }}
                         >
-                            <List sx={{ border: '1px solid black' }}>
+                            <List>
                                 <ListItem >
                                     <ListItemButton>
                                         <ListItemIcon>
-                                            <InboxIcon />
+                                            <PixIcon />
                                         </ListItemIcon>
                                         <ListItemText primary="CATEGORIES" />
                                     </ListItemButton>
@@ -36,7 +39,7 @@ const Dashboard = () => {
                                         <ListItemText primary="ASSET LOGS" />
                                     </ListItemButton>
                                 </ListItem>
-                                <ListItem sx={{border:'1px solid red',borderRadius:'10px'}}>
+                                <ListItem sx={{ background: '#1D3557', borderRadius: '10px', color: '#fff' }}>
                                     <ListItemButton>
                                         <ListItemIcon>
                                             <DraftsIcon />
@@ -51,7 +54,77 @@ const Dashboard = () => {
                     </Container>
                 </Grid>
                 <Grid item lg={9}>
+                    <Stack>
+                        <Grid container mt={3}>
+                            <Grid item>
+                                <Typography variant="subtitle" component="p" textAlign='start'>
+                                    Welcome
+                                </Typography>
+                                <Typography variant="h4" component="p" sx={{ color: '#fff' }}>
+                                    Hi Debasish
+                                </Typography>
+                            </Grid>
+                        </Grid>
 
+                    </Stack>
+                    <Divider textAlign="left" sx={{ color: '#fff', marginTop: 2 }}>CATEGORIES</Divider>
+
+                    {/* Categories Stack */}
+                    <Stack mt={3} spacing={2}>
+                        <Grid container spacing={2} >
+                            <Grid item sx={{}} >
+                                <Card sx={{ width: 150, height: 150, }}>
+                                    <Box sx={{ borderTop: '14px solid black' }} />
+                                    <Grid container sx={{height:100}} alignItems='center' justifyContent='center'>
+                                    <Typography alignItems='center'>
+                                        fff
+                                    </Typography>
+                                    </Grid>
+                                    
+
+                                </Card>
+                            </Grid>
+                            <Grid item sx={{}} >
+                                <Card sx={{ width: 150, height: 150, }}>
+                                    <Box sx={{ borderTop: '14px solid black' }} />
+                                    <Grid container sx={{height:100}} alignItems='center' justifyContent='center'>
+                                    <Typography alignItems='center'>
+                                        fff
+                                    </Typography>
+                                    </Grid>
+                                    
+
+                                </Card>
+                            </Grid>
+                            <Grid item sx={{}} >
+                                <Card sx={{ width: 150, height: 150, }}>
+                                    <Box sx={{ borderTop: '14px solid black' }} />
+                                    <Grid container sx={{height:100}} alignItems='center' justifyContent='center'>
+                                    <Typography alignItems='center'>
+                                        fff
+                                    </Typography>
+                                    </Grid>
+                                    
+
+                                </Card>
+                            </Grid>
+                            <Grid item sx={{}} >
+                                <Card sx={{ width: 150, height: 150,border:'2px dashed black' }}>
+                                    <Box  />
+                                    <Grid container sx={{height:100}} alignItems='center' justifyContent='center'>
+                                    <Typography>
+                                        fff
+                                    </Typography>
+                                    </Grid>
+                                    
+
+                                </Card>
+                            </Grid>
+                            
+
+                        </Grid>
+
+                    </Stack>
                 </Grid>
             </Grid>
 
