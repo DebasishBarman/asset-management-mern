26c61699a401d1eeffca10004ee97185e2497a7e
Debasish Barman <debdeva298@gmail.com>
2021-11-26 22:48:06 +0530

    Routes added without Authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
-
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      {/* <Login/> */}
-      <Dashboard/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
+    </Router>
   );
 }
 
