6dee4c8f13982089d1ce55c72ade6b5635cfc164
Debasish Barman <debdeva298@gmail.com>
2021-11-28 16:12:31 +0530

    Drawer Design

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import {
-    Container, Grid, Stack, Divider, Typography, Drawer, Button,
-    Card, List, ListItem, ListItemButton, ListItemIcon, Box, Tab, Tabs, PropTypes
+    Container, Grid, Stack, Divider, Typography, Drawer, Button, Select, MenuItem,FormControl,InputLabel,
+    Card,Box, Tab, Tabs, PropTypes, TextField, Autocomplete
 } from '@mui/material'
 
 const Categories = () => {
     const [state, setState] = React.useState({ right: false });
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
 
     const toggleDrawer = (anchor, open) => (event) => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -83,9 +88,52 @@ const Categories = () => {
                 >
                     <Box
                         sx={{ width: 'right' === 'top' || 'right' === 'bottom' ? 'auto' : 450 }}
-                        onClick={toggleDrawer('right', false)}
+                    // onClick={toggleDrawer('right', false)}
                     >
-                        <p>Hello</p>
+                        <Grid container lg={12} alignItems='center'>
+                            <Grid items p={2} lg={12} p={2}>
+                                <TextField
+                                    label='Category name'
+                                    variant='standard'
+                                    fullWidth />
+                            </Grid>
+                            <Grid items p={2} lg={12}>
+                                <TextField
+                                    label='Asset ID'
+                                    variant='standard'
+                                    fullWidth />
+                            </Grid>
+                            <Grid items p={2} lg={12}>
+                                <TextField
+                                    type='date'
+                                    variant='outlined'
+                                    // label='Date'
+                                    fullWidth />
+                            </Grid>
+                            <Grid items lg={12} p={2}>
+                                <FormControl variant="standard" sx={{width:'100%'}} >
+                                    <InputLabel id="demo-simple-select-standard-label">Asset Status</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-standard-label"
+                                        id="demo-simple-select-standard"
+                                        value={age}
+                                        onChange={handleChange}
+                                        label="Asset Status"
+                                        sx={{width:'100%'}}
+                                    >
+                                        
+                                        <MenuItem value='active'>Active</MenuItem>
+                                        <MenuItem value='submitted'>Submitted</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid items p={2} lg={12}>
+                                <TextField
+                                    label='Asset Assigned to'
+                                    variant='standard'
+                                    fullWidth />
+                            </Grid>
+                        </Grid>
 
                     </Box>
                 </Drawer>
